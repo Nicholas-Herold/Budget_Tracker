296dda908b8ffe934fe3ae412f9a214f87a62262
Nicholas Herold <nherold86@gmail.com>
2021-09-18 22:57:00 -0400

    bug fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,75 +1,64 @@
 const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    './assets/js/index.js',
-    "./assets/style/style.css",
-    "./assets/icons/icon-192x192.png",
-    "./assets/icons/icon-512x512.png",
-    "./manifest.json",
-    "/favicon.ico"
+    "/index.html",
+    "/js/index.js",
+    "/style/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/manifest.webmanifest",
+    
 ];
 
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
+self.addEventListener('install', (event) => {
+  console.log('SW is installing');
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(function(){console.log("SW install complete")})
+      .then(self.skipWaiting())
+  );
+});
 
-
-self.addEventListener("install", function(evt) {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.addAll(FILES_TO_CACHE);
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
       })
-    );
-  
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
+      .then((cachesToDelete) => {
         return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              return caches.delete(key);
-            }
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
           })
         );
       })
-    );
-  
-    self.clients.claim();
-  });
-  
-  // fetch
-  self.addEventListener("fetch", function(evt) {
-    // cache successful requests to the API
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-  
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
               return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
             });
-        }).catch(err => console.log(err))
-      );
-  
-      return;
-    }
-  
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    evt.respondWith(
-      caches.match(evt.request).then(function(response) {
-        return response || fetch(evt.request);
+          });
+        });
       })
     );
-  });
+  }
+});
